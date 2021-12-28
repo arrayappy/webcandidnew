f79bc677f21690d656f231e5a6cd8dbe351144c0
kandurisv <30374472+kandurisv@users.noreply.github.com>
2021-12-28 11:37:07 +0530

    Almost all changes are done

---
+++ Components/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import LandingScreen1 from './LandingScreen1'
+import LandingScreen2 from './LandingScreen2'
+import LandingScreen3 from './LandingScreen3'
+
+const LandingPage = ({content}) => {
+    return (
+        <div>
+           <LandingScreen1 />
+           <LandingScreen3 />
+        </div>
+    )
+}
+
+export default LandingPage
---
+++ utils/exports.js
@@ -0,0 +1,1 @@
+export const API = "https://sim4yarfg6.execute-api.ap-south-1.amazonaws.com/dev/"
